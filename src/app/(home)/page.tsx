0a60791fc2b4c4837e84ec6d7dfa655546956f1c
biswajit <ascebiswajit@gmail.com>
2024-06-04 23:16:19 +0530

    group the home file

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,14 @@
+// import Banner from "./components/Banner";
+// you can do it both way
+import Banner from "@/app/(home)/components/Banner";
+
+
+export default function Home() {
+  return (
+    <>
+      <div>
+        <Banner />
+      </div>
+    </>
+  );
+}
