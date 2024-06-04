31b18088f0438adb86d8c91274b5b618192c143d
biswajit <ascebiswajit@gmail.com>
2024-06-04 23:03:34 +0530

    Add Banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
+import Banner from "@/components/Banner";
 import Image from "next/image";
 
 export default function Home() {
   return (
 <>
-<h1>
-  Welocome to E-Book Client app
-</h1>
+
+<div>
+  <Banner/>
+</div>
 </>
   );
 }
